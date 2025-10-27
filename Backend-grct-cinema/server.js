6f975b5c2cec9c45ac2880bd7eba476624ea8c06
DjakoMal <rafioudjakomallo00@gmail.com>
2025-10-27 15:07:57 +0000

    add .env

--- Backend-grct-cinema/server.js
+++ Backend-grct-cinema/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+// config db 
 const connectDB = require('./Config/db');
 
 // Charger les variables d'environnement
